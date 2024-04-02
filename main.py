61e5a0f66b44e17b8042a521f066ca8b7b206fb6
Eric Kenji <forspecialfiap@gmail.com>
2024-04-02 18:45:00 -0300

    Created the variables colors, genius_bot and score

--- main.py
+++ main.py
@@ -2,7 +2,18 @@ import os
 import random
 import time
 
-#Clear screen logic
+# In Brazil, we call Simon Game as Genius, so i'll use the name Genius here
+
+# Clear screen logic
+# In case the OS is Windows, it will clear the screen with cls, otherwise it will use clear (NOTE: In Windows 11, both will work)
 def clear():
-    #In case the OS is Windows, it will clear the screen with cls, otherwise it will use clear (NOTE: In Windows 11, both will work)
-    os.system('cls' if os.name == 'nt' else 'clear')
+    os.system("cls" if os.name == "nt" else "clear")
+
+# Usable colors
+colors = ("R", "B", "Y", "G")
+
+# Genius Bot
+genius_bot = []
+
+# Player Score
+score = 0
